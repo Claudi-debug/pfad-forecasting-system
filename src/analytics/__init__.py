8d8073f813dae6bb86eb9dd9940143dbd34c8f42
Claudi-debug <prasanthscma2021@gmail.com>
2025-06-19 17:30:46 +0530

    Add analytics module init file

---
+++ src/analytics/__init__.py
@@ -0,0 +1,13 @@
+"""Advanced Econometric Analytics Module
+
+Professional econometric models for PFAD price analysis including:
+- Vector Autoregression (VAR) models
+- GARCH volatility modeling
+- Granger causality testing
+- Cointegration analysis
+"""
+
+from .advanced_econometric_engine import PFADEconometricEngine
+
+__all__ = ["PFADEconometricEngine"]
+__version__ = "2.0.0"
